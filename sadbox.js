63efc962247dcce5e364bfb3e60bbaea84b380e3
Damilola <Damilola2554@gmail.com>
2025-04-29 10:44:46 +0100

    learnt about Equality operators and logical operators

--- sadbox.js
+++ sadbox.js
@@ -228,50 +228,98 @@ GOOD LUCK 😀
 //   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
 // }
 
+// ////////////////////////////////////
+// // Type Conversion and Coercion
+
+// // type conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// // type coercion
+// console.log('I am ' + 23 + ' years old'); to string
+// console.log('23' - '10' - 3); to number
+// console.log('23' / '2'); to number
+// console.log('23' * '2'); to number
+// console.log('23' > '18'); convert to boolone
+
+// let n = "1" + 1; // '11'
+// n = n - 1;
+// console.log(n);
+
+// ////////////////////////////////////
+// // Truthy and Falsy Values
+
+// // 5 falsy values: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 100;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 0;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is UNDEFINED");
+// }
+
 ////////////////////////////////////
-// Type Conversion and Coercion
+// Equality Operators: == vs. ===
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
 
-// type conversion
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+if (age == 18) console.log('You just became an adult :D (loose)');
 
-console.log(Number("Jonas"));
-console.log(typeof NaN);
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
 
-console.log(String(23), 23);
-
-// type coercion
-console.log('I am ' + 23 + ' years old'); to string
-console.log('23' - '10' - 3); to number
-console.log('23' / '2'); to number
-console.log('23' * '2'); to number
-console.log('23' > '18'); convert to boolone
-
-let n = "1" + 1; // '11'
-n = n - 1;
-console.log(n);
-
-////////////////////////////////////
-// Truthy and Falsy Values
-
-// 5 falsy values: 0, '', undefined, null, NaN
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("Jonas"));
-console.log(Boolean({}));
-console.log(Boolean(""));
-
-const money = 100;
-if (money) {
-  console.log("Don't spend it all ;)");
+if (favourite === 23) { // 22 === 23 -> FALSE
+  console.log('Cool! 23 is an amzaing number!')
+} else if (favourite === 7) {
+  console.log('7 is also a cool number')
+} else if (favourite === 9) {
+  console.log('9 is also a cool number')
 } else {
-  console.log("You should get a job!");
+  console.log('Number is not 23 or 7 or 9')
 }
 
-let height = 0;
-if (height) {
-  console.log("YAY! Height is defined");
+if (favourite !== 23) console.log('Why not 23?');
+
+////////////////////////////////////
+// Logical Operators
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
 } else {
-  console.log("Height is UNDEFINED");
+  console.log('Someone else should drive...');
 }
+*/
